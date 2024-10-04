52c4ddc508fb7029adeda860cc0bf224a1bc25a0
Antes <coderasylum@gmail.com>
2024-10-04 22:20:50 +0300

    feat(hw №2): add parsing from site and groups of dependencies

--- Homeworks/Homework №2/main.py
+++ Homeworks/Homework №2/main.py
@@ -1,21 +1,25 @@
 import os
 import sys
 import subprocess
+import requests
 
+from bs4 import BeautifulSoup
 from shutil import rmtree
 from zipfile import ZipFile 
 from xml.etree import ElementTree
 
 class DependencyDrawer:
-    def __init__(self, 
-                 config_file_path: str) -> None:
-        
-        self.config_paht = config_file_path
+    def __init__(self, config_file_path: str) -> None:
         self.ns = {'nuspec': 'http://schemas.microsoft.com/packaging/2013/05/nuspec.xsd'}
-        self.__load_config()
+        self.graphviz_path = None
+        self.package_url = None
+        self.__load_config(config_file_path)
 
-    def __load_config(self) -> tuple[str, str]:
-        tree = ElementTree.parse(self.config_paht)
+    def __load_config(self, config_file_path: str) -> tuple[str, str]:
+        if not os.path.isfile(config_file_path):
+            raise Exception('Configuration file not found')
+
+        tree = ElementTree.parse(config_file_path)
         root = tree.getroot()
 
         graphviz_path = root.find('GraphvizPath').text
@@ -23,42 +27,47 @@ class DependencyDrawer:
             graphviz_path = 'dot'
         self.graphviz_path = graphviz_path
 
-        package_path = root.find('PackagePath').text
-        if not os.path.isfile(package_path):
-            raise Exception("Package file not found")
-        self.package_path = package_path
+        package_url = root.find('PackagePath').text
+        self.package_url = package_url
 
-    def __analyze(self) -> tuple[str, str, list[tuple[str, str]]]:
-        with ZipFile(self.package_path, 'r') as zip_ref:
-            zip_ref.extractall('extracted_package')
+    def __parse(self) -> dict[str: tuple[str, str]]:
+        response = requests.get(self.package_url + '#dependencies-body-tab')
+        if response.status_code != 200:
+            raise Exception('HTML page parse error')
         
-        nuspec_file = [f for f in os.listdir('extracted_package') if f.endswith('.nuspec')][0]
-        tree = ElementTree.parse(os.path.join('extracted_package', nuspec_file))
-        root = tree.getroot()
+        html_content = response.text
+        soup = BeautifulSoup(html_content, 'html.parser')
+        package_name = soup.find('span', {'class': 'title'}).text.strip()
+        package_version = soup.find('span', {'class': 'version-title'}).text.strip()
+        dependencies_div = soup.find('ul', {'id': 'dependency-groups'})
 
-        package_id = root.find("nuspec:metadata/nuspec:id", self.ns).text
-        package_version = root.find("nuspec:metadata/nuspec:version", self.ns).text
-        dependencies = []
-        for group in root.findall(".//nuspec:group", self.ns):
-            for dependency in group.findall("nuspec:dependency", self.ns):
-                dep_name = dependency.attrib['id']
-                dep_version = dependency.attrib['version']
-                dependencies.append((dep_name, dep_version))
-        
-        rmtree('extracted_package')
+        dependencies = dict()
+        dependencies_rows = dependencies_div.findAll('li', recursive=False)
+        for dependency_row in dependencies_rows:
+            dependencies_group = dependency_row.find('h4').find('span').text.strip()
+            dependencies_list = dependency_row.find('ul', recursive=False).findAll('li', recursive=False)
+            for dependency in dependencies_list:
+                dependency_name = dependency.find('a')
+                if dependency_name:
+                    dependency_name = dependency_name.text.strip()
+                    dependency_version = dependency.find('span').text.strip()
+                    dependencies.setdefault(dependencies_group, set()).add((dependency_name, dependency_version))
 
-        return package_id, package_version, dependencies
+        return package_name, package_version, dependencies
     
     def __render(self, 
                  package_name: str, 
                  package_version: str, 
-                 dependencies: list[tuple[str, str]]) -> None:
+                 dependencies: dict[str: tuple[str, str]]) -> None:
         
         dot_graph = 'digraph G {\n'
-        dot_graph += f'    "{package_name}" [label="{package_name}\\n{package_version}"];\n'
-        for dep_name, dep_version in set(dependencies):
-            dot_graph += f'    "{dep_name}" [label="{dep_name}\\n{dep_version}"];\n'
-            dot_graph += f'    "{package_name}" -> "{dep_name}";\n'
+        dot_graph += f'    "{package_name}" [label="{package_name}\\n{package_version}", shape=box, style=filled, fillcolor=lightblue];\n'
+        for group, dependencies in dependencies.items():
+            dot_graph += f'    "{group}" [shape=ellipse, style=filled, fillcolor=lightgray];\n'
+            dot_graph += f'    "{package_name}" -> "{group}";\n'
+            for dep_name, dep_version in dependencies:
+                dot_graph += f'    "{dep_name}" [label="{dep_name}\\n{dep_version}", shape=box];\n'
+                dot_graph += f'    "{group}" -> "{dep_name}";\n'
         dot_graph += '}\n'
         
         with open(f'dependencies.dot', 'w') as f:
@@ -67,7 +76,7 @@ class DependencyDrawer:
         subprocess.run([self.graphviz_path, '-Tpng', 'dependencies.dot', '-o', f'dependencies.png'])
 
     def run(self) -> None:
-        package_name, package_version, dependencies = self.__analyze()
+        package_name, package_version, dependencies = self.__parse()
         self.__render(package_name, package_version, dependencies)
         print("Drawed successfully!")
 
